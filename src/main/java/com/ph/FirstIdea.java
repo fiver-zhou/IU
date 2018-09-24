73fbe859d03b2dbe7e981c24fb222080fc074de8
fiver <1371408505@qq.com>
2018-09-24 16:52:00 +0800

    first commit

---
+++ src/main/java/com/ph/FirstIdea.java
@@ -0,0 +1,10 @@
+package com.ph;
+
+/**
+ * Created by fiver on 2018/9/24.
+ */
+public class FirstIdea {
+    public static void main(String[] args) {
+        System.out.println("haha");
+    }
+}
