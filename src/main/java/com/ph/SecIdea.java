d6ac19883b63af3d3596dc3599c9359e2287ebb9
fiver <1371408505@qq.com>
2018-09-24 16:55:14 +0800

    sec commit

---
+++ src/main/java/com/ph/SecIdea.java
@@ -0,0 +1,12 @@
+package com.ph;
+
+/**
+ * Created by fiver on 2018/9/24.
+ */
+public class SecIdea {
+    public static void main(String[] args) {
+        System.out.println("haha");
+        int a = 1;
+        int b = a;
+    }
+}
